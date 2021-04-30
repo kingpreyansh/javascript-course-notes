52b8798992bb5422da959fc3c3013b1018491c97
Preyansh Dutta <41342405+kingpreyansh@users.noreply.github.com>
2021-04-30 17:20:48 -0400

    Update and rename JavaScript.txt to JavaScript.js

--- Desktop/JavaScript.js
+++ Desktop/JavaScript.js
@@ -4,11 +4,50 @@ alert("Hello World!")
 console.log
 # outputs any calculation or text on the console
 
+
+/*
 7 Primitive Data Types
 => Number: Floating point numbers
 => String: Sequence of characters
 => Boolean: Logical type that can only be true or false
-=>
-=>
-=>
-=>
+=> Undefined: Value taken by a variable that is not yet defined
+=> Null: Also means 'empty value'
+=> Symbol (ES2015)
+=> BigInt (ES2020)
+*/
+
+// This is a comment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
