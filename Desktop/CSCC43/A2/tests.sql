8a7389a470897bf12c79ce309c15249c8cf5306c
kingpreyansh <preyanshdutta@gmail.com>
2022-11-06 17:32:02 -0500

    Adding more tests

--- Desktop/CSCC43/A2/tests.sql
+++ Desktop/CSCC43/A2/tests.sql
@@ -1,3 +1,8 @@
+-- Steps
+-- run psql with default host user and pass
+-- create database a2
+
+
 -- The Player table contains information about the players of the game.
 -- 'id' is the id of the player.
 -- 'playername' is the chosen playername of the player.
@@ -9,6 +14,29 @@
 -- 'losses' is the number of times the player has lost a completed battle. You can assume this is always >= 0.
 -- 'total_battles' is the number of times the player has participated in a (not necessarily completed) battle. You can assume this is always >= 0. The reason why wins plus losses is not necessarily equal to total_battles is that there may be some unforeseen circumstances that lead to a battle not being completed (eg: server or connection issues).
 -- 'guild' is the id of the guild that the player belongs to (whether they are member or a leader) if they belong to one, NULL otherwise. A player can only belong to at most one guild. The constraint on this column can be found after the definition of the Guild table.
+
+/*
+Lilmon table
+    id INTEGER PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL,
+    element1 VARCHAR NOT NULL,
+    element2 VARCHAR,
+    rarity INTEGER NOT NULL,
+    CHECK (element1 IN ('Water', 'Fire', 'Air', 'Earth', 'Ice', 'Electric', 'Light', 'Dark')),
+    CHECK (element2 IN ('Water', 'Fire', 'Air', 'Earth', 'Ice', 'Electric', 'Light', 'Dark')),
+    CHECK (element2 != element1),
+    CHECK (rarity BETWEEN 3 AND 5)
+*/
+-- Insert the 8 different lilmons into the Lilmon table.
+INSERT INTO Lilmon (id, name, element1, element2, rarity) VALUES
+(1, 'Watermon', 'Water', NULL, 3),
+(2, 'Firemon', 'Fire', 'Water', 4),
+(3, 'Airmon', 'Air', 'Earth', 5),
+(4, 'Earthmon', 'Earth', NULL, 5),
+(5, 'Icemon', 'Ice', NULL, 5),
+(6, 'Electricmon', 'Electric', NULL, 3),
+(7, 'Lightmon', 'Light', NULL, 4),
+(8, 'Darkmon', 'Dark', NULL, 3);
 
 
 /*
@@ -25,15 +53,14 @@ Player Attributes
     guild INTEGER
 */
 
-
 -- Insert 10 different players with ids from 1 to 10 different attributes into the Player table.
 INSERT INTO Player (id, playername, email, country_code, coins, rolls, wins, losses, total_battles, guild)
-VALUES (1, 'player1', 'player1@gmail.com', '333', 5, 6, 7, 8, 19, 1),
-       (2, 'player2', 'player2@gmail.com', '334', 5, 6, 7, 8, 25, 2),
-       (3, 'player3', 'player3@gmail.com', '335', 5, 6, 7, 8, 37, 3),
-       (4, 'player4', 'player4@gmail.com', '336', 2, 6, 9, 50, 82, 4),
-       (5, 'player5', 'player5@gmail.com', '336', 2, 100, 100, 8, 199, 5),
-       (6, 'player6', 'player6@gmail.com', '333', 100000, 40, 19, 40, 70, 6);
+VALUES (1, 'player1', 'player1@gmail.com', 'CAN', 5, 6, 7, 8, 19, 1),
+       (2, 'player2', 'player2@gmail.com', 'MEX', 5, 6, 7, 8, 25, 2),
+       (3, 'player3', 'player3@gmail.com', 'USA', 5, 6, 7, 8, 37, 3),
+       (4, 'player4', 'player4@gmail.com', 'CAN', 2, 6, 9, 50, 82, 4),
+       (5, 'player5', 'player5@gmail.com', 'MEX', 2, 100, 100, 8, 199, 5),
+       (6, 'player6', 'player6@gmail.com', 'MYS', 100000, 40, 19, 40, 70, 6);
 
 
 /*
@@ -45,7 +72,34 @@ Guild Attributes
 */
 
 INSERT INTO Guild(id, guildname, tag, leader)
-VALUES (1, 'Guild1', 'G1', 1), (2, 'Guild2', 'G2', 2), (3, 'Guild3', 'G3', 3), (4, 'Guild4', 'G4', 4), (5, 'Guild5', 'G5', 5), (6, 'Guild6', 'G6', 6), (7, 'Guild7', 'G7', 7);
+VALUES (1, 'Guild1', 'G1', 1), (2, 'Guild2', 'G2', 2), (3, 'Guild3', 'G3', 3), (4, 'Guild4', 'G4', 4), (5, 'Guild5', 'G5', 5), (6, 'Guild6', 'G6', 6);
+
+/*
+GuildRatings
+    id SERIAL PRIMARY KEY,
+        g_id INTEGER NOT NULL REFERENCES Guild(id) ON DELETE RESTRICT,
+        month INTEGER NOT NULL,
+        year INTEGER NOT NULL,
+        monthly_rating INTEGER NOT NULL,
+        all_time_rating INTEGER NOT NULL,
+        CHECK (month BETWEEN 1 AND 12)
+*/
+-- guild id's should be from 1 to 10
+-- month and years should be different
+-- monthly_rating and all_time_rating should be different and should be from 1 to 100
+INSERT INTO GuildRatings(id, g_id, month, year, monthly_rating, all_time_rating)
+VALUES (100, 1, 1, 2020, 1, 5000),
+        (101, 1, 2, 2020, 50, 5000),
+        (102, 1, 3, 2020, 100, 5000),
+        (103, 1, 4, 2020, 150, 5000),
+       (200, 2, 1, 2020, 400, 4000),
+       (201, 2, 2, 2020, 550, 4000),
+       (202, 2, 5, 2020, 500, 4000),
+       (300, 3, 3, 2020, 3, 6000),
+       (400, 4, 4, 2020, 4, 2000),
+       (401, 4, 5, 2020, 750, 2000),
+       (500, 5, 5, 2020, 5, 450),
+       (600, 6, 6, 2020, 6, 7000);
 
 /*
 Lilmon Inventory
@@ -55,8 +109,21 @@ Lilmon Inventory
       in_team BOOLEAN NOT NULL DEFAULT FALSE,
       fav BOOLEAN NOT NULL DEFAULT FALSE
 */
-INSERT INTO Lilmon_Inventory(l_id, p_id, in_team, fav)
-VALUES (1, 1, TRUE, TRUE), (2, 1, FALSE, FALSE), (3, 1, FALSE, FALSE), (4, 1, FALSE, FALSE), (5, 1, FALSE, FALSE), (6, 1, FALSE, FALSE), (7, 1, FALSE, FALSE), (8, 1, FALSE, FALSE), (9, 1, FALSE, FALSE), (10, 1, FALSE, FALSE), (11, 1, FALSE, FALSE), (12, 1, FALSE, FALSE), (13, 1, FALSE, FALSE), (14, 1, FALSE, FALSE), (15, 1, FALSE, FALSE), (16, 1, FALSE, FALSE), (17, 1, FALSE, FALSE), (18, 1, FALSE, FALSE), (19, 1, FALSE, FALSE), (20, 1, FALSE, FALSE), (21, 1, FALSE, FALSE), (22, 1, FALSE, FALSE), (23, 1, FALSE, FALSE), (24, 1, FALSE, FALSE), (25, 1, FALSE, FALSE), (26, 1, FALSE, FALSE), (27, 1, FALSE, FALSE), (28, 1, FALSE, FALSE), (29, 1, FALSE, FALSE), (30, 1, FALSE, FALSE), (31, 1, FALSE, FALSE), (32, 1, FALSE, FALSE), (33, 1, FALSE, FALSE), (34, 1, FALSE, FALSE), (35, 1, FALSE, FALSE), (36, 1, FALSE, FALSE), (37, 1, FALSE, FALSE), (38, 1, FALSE, FALSE), (39, 1, FALSE, FALSE), (40, 1, FALSE, FALSE), (41, 1, FALSE, FALSE), (42, 1, FALSE, FALSE), (43, 1, FALSE, FALSE), (44, 1, FALSE, FALSE), (45, 1, FALSE, FALSE), (46, 1, FALSE, FALSE), (47, 1, FALSE, FALSE), (48, 1, FALSE, FALSE), (49, 1, FALSE, FALSE), (50, 1, FALSE, TRUE);
+-- Insert 10 lilmons with different p_id's from 1 to 6
+INSERT INTO Lilmon_Inventory(id, l_id, p_id, in_team, fav)
+VALUES (100, 1, 1, TRUE, FALSE),
+       (200, 2, 1, TRUE, TRUE),
+       (300, 3, 1, FALSE, TRUE),
+       (301, 4, 1, FALSE, TRUE),
+       (301, 5, 1, FALSE, TRUE),
+       (400, 4, 2, TRUE, FALSE),
+       (500, 5, 2, TRUE, TRUE),
+       (600, 6, 3, TRUE, TRUE),
+       (700, 7, 4, FALSE, TRUE),
+       (800, 8, 5, TRUE, FALSE),
+       (900, 1, 6, TRUE, FALSE),
+       (101, 2, 6, TRUE, TRUE);
+
 
 /*
 PlayerRatings
@@ -72,20 +139,14 @@ PlayerRatings
 -- month and years should be different
 -- monthly_rating and all_time_rating should be different and should be from 1 to 100
 INSERT INTO PlayerRatings(p_id, month, year, monthly_rating, all_time_rating)
-VALUES (1, 1, 2019, 1, 1), (2, 2, 2019, 2, 2), (3, 3, 2019, 3, 3), (4, 4, 2019, 4, 4), (5, 5, 2019, 5, 5), (6, 6, 2019, 6, 6), (7, 7, 2019, 7, 7), (8, 8, 2019, 8, 8), (9, 9, 2019, 9, 9), (10, 10, 2019, 10, 10);
+VALUES (1, 1, 2019, 1, 2000),
+       (2, 2, 2019, 2, 2500),
+       (3, 3, 2019, 3, 5000),
+       (4, 4, 2019, 4, 350),
+       (5, 5, 2019, 5, 600),
+       (6, 6, 2019, 6, 2000),
+       (6, 7, 2019, 7, 2000),
+       (6, 8, 2019, 8, 2000),
+       (3, 9, 2019, 9, 5000),
+       (3, 10, 2019, 10, 5000);
 
-/*
-GuildRatings
-    id SERIAL PRIMARY KEY,
-        g_id INTEGER NOT NULL REFERENCES Guild(id) ON DELETE RESTRICT,
-        month INTEGER NOT NULL,
-        year INTEGER NOT NULL,
-        monthly_rating INTEGER NOT NULL,
-        all_time_rating INTEGER NOT NULL,
-        CHECK (month BETWEEN 1 AND 12)
-*/
--- guild id's should be from 1 to 10
--- month and years should be different
--- monthly_rating and all_time_rating should be different and should be from 1 to 100
-INSERT INTO GuildRatings(g_id, month, year, monthly_rating, all_time_rating)
-VALUES (1, 1, 2019, 1, 1), (2, 2, 2019, 2, 2), (3, 3, 2019, 3, 3), (4, 4, 2019, 4, 4), (5, 5, 2019, 5, 5), (6, 6, 2019, 6, 6), (7, 7, 2019, 7, 7), (8, 8, 2019, 8, 8), (9, 9, 2019, 9, 9), (10, 10, 2019, 10, 10);
